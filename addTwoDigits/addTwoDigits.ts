45252dc2ae6c55fbec4addce1f824d58e968aa52
SimasDei <simondeiman@gmail.com>
2019-02-14 18:48:28 +0200

    absolute Values, add, addBorder, addTwoDigits, adjacentElement

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,5 @@
 function addTwoDigits(n: any): number {
-
+  const numbers = n.toString().split('');
+  return +numbers[0] + +numbers[1];
 }
-
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
