e28fa5a2c49b0b0ac11aeacc6e9f32a1304c2feb
SimasDei <simondeiman@gmail.com>
2019-02-16 17:42:11 +0200

    array Max Consecutive Sum. Array Max Adjacent Difference, Array Previous Less

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,14 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+  let maxDiff = Math.abs(inputArray[0] - inputArray[1]);
 
+  for (let i = 0; i < inputArray.length; i++) {
+    const absoluteDiff = Math.abs(inputArray[i - 1] - inputArray[i]);
+    if (absoluteDiff > maxDiff) {
+      maxDiff = absoluteDiff;
+    }
+  }
+
+  return maxDiff;
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
