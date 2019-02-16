ba4d5e3e93274a718559517744645732288a8359
SimasDei <simondeiman@gmail.com>
2019-02-16 16:48:48 +0200

    longest string, almost increase, alphabetical shift

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,15 @@
 function allLongestStrings(inputArray: string[]): string[] {
-
+  let longestString = 0;
+  const longestWords = [];
+  inputArray.forEach(word => {
+    longestString = longestString < word.length ? word.length : longestString;
+  });
+  inputArray.forEach((word: string) => {
+    if (word.length === longestString) {
+      longestWords.push(word);
+    }
+  });
+  return longestWords;
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(['aba', 'aaf', 'ad', 'vcd', 'aba']));
--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,42 @@
 function alphabeticShift(inputString: string): string {
+  const alphabet: string[] = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
 
+  let inputShifted = inputString.split('');
+  for (let i = 0; i < inputShifted.length; i++) {
+    let index = 0;
+    if (inputShifted[i] !== 'z') {
+      index = alphabet.indexOf(inputShifted[i]) + 1;
+    }
+    inputShifted[i] = alphabet[index];
+  }
+  return inputShifted.join('');
 }
 
 console.log(alphabeticShift('crazy'));
