b3c9e0eb47ba840a643300f3cc43fd376d7c03a4
SimasDei <simondeiman@gmail.com>
2019-02-16 17:04:00 +0200

    aphabet sub, alternating sums, are equaly strong

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,26 @@
 function alphabetSubsequence(s: string): boolean {
+  const chars: string[] = s.split('');
+  const charValues: number[] = [];
 
+  chars.forEach((char: string) => {
+    charValues.push(char.charCodeAt(0));
+  });
+
+  if (new Set(charValues).size !== charValues.length) {
+    return false;
+  }
+
+  for (let i = 0; i < charValues.length - 1; i++) {
+    if (charValues[i] >= charValues[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence('zab'));
+console.log(alphabetSubsequence('effg'));
+console.log(alphabetSubsequence('cdce'));
+console.log(alphabetSubsequence('ace'));
+console.log(alphabetSubsequence('bxz'));
--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,19 @@
-function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+  const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+  const friendsWeakest =
+    friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+  const friendsStrongest =
+    friendsRight >= friendsLeft ? friendsLeft : friendsRight;
 
+  return yourStrongest == friendsStrongest && yourWeakest === friendsWeakest;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
