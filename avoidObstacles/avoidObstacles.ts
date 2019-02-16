48c3ef88e8b6778b967e9fcc9693a626e19d25cb
SimasDei <simondeiman@gmail.com>
2019-02-16 17:56:51 +0200

    Array replace, Avoid Obstacles, Bishop and Pawn

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,12 @@
 function avoidObstacles(inputArray: number[]): number {
+  inputArray = inputArray.sort((a, b) => a - b);
+  const largestArrayVal = inputArray[inputArray.length - 1];
 
+  for (let i = 1; i <= largestArrayVal + 1; i++) {
+    if (inputArray.every(element => element % i !== 0)) {
+      return i;
+    }
+  }
 }
 
 console.log(avoidObstacles([5, 3, 6, 7, 9]));
