d08c61071bc18905fa0ac40c2b5d1e452b5eded9
SimasDei <simondeiman@gmail.com>
2019-02-25 19:36:36 +0200

    chess Board, Chunky Monkey, circle of Numbers

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,5 +1,19 @@
 function chessBoardCellColor(cell1: string, cell2: string): boolean {
+  const board = {
+    A: 1,
+    B: 2,
+    C: 3,
+    D: 4,
+    E: 5,
+    F: 6,
+    G: 7,
+    H: 8
+  };
 
+  const total = board[cell1[0]] + parseInt(cell1[1]);
+  const cellTotal = board[cell2[0]] + parseInt(cell2[1]);
+
+  return total % 2 === cellTotal % 2;
 }
 
 console.log(chessBoardCellColor('A1', 'C3'));
--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,12 @@
 function chunkyMonkey(arr: any[], size: number): any[][] {
+  const nested = [];
+  let count = 0;
 
+  while (count < arr.length) {
+    nested.push(arr.slice(count, (count += size)));
+  }
+  return nested;
 }
 
-console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey(['a', 'b', 'c', 'd'], 2));
 console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
