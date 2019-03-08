8e42e661a5f75f694b3cb80b762d61fe27f87b13
SimasDei <simondeiman@gmail.com>
2019-03-08 10:03:54 +0200

    crossing Sum, Deposit Profit, Different Symbols

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,15 @@
-function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+function depositProfit(
+  deposit: number,
+  rate: number,
+  threshold: number
+): number {
+  let year = 0;
+  let account = deposit;
+  while (threshold > account) {
+    account += account * (rate / 100);
+    year++;
+  }
+  return year;
 }
 
-console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 29, 399));
--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,14 @@
 function differentSymbolsNaive(s: string): number {
- 
+  const uniqueChars: string[] = [];
+  const inputChars: string[] = s.split('');
+
+  inputChars.forEach(char => {
+    if (!uniqueChars.includes(char)) {
+      uniqueChars.push(char);
+    }
+  });
+
+  return uniqueChars.length;
 }
 
 console.log(differentSymbolsNaive('cabca'));
